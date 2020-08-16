7a31979bc07e4e77bacdd482f90b8c202f885a94
mohleo <mohleo.nocturnal@gmail.com>
2020-08-16 22:32:02 +0530

    pushing mohleo consultancy to gitHub

---
+++ js/main.js
@@ -0,0 +1,30 @@
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.querySelector('.map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
+  // window.addEventListener("scroll",function(){
+  //   if(window.scrollY>100){
+  //     document.querySelector("#navbar").style.opacity=0.9;}
+  //     else{
+  //       document.querySelector("#navbar").style.opacity=1;
+  //     }
+  //   });
+
+  $('#navbar a, .btn').on('click', function(event){
+    if(this.hash !==""){
+      event.preventDefault();
+      const hash=this.hash;
+      $('html,body').animate(
+        {
+          scrollTop: $(hash).offset().top-50
+        },800
+      );
+    }
+  });
